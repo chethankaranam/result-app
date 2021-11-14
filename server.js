b2241255e63dbbe521208264ffc11ceecbba438d
Chethan Sai K <chethansaikaranam@gmail.com>
2021-11-14 14:31:47 +0530

    Heroku requirements

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/result-app'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/result-app/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
